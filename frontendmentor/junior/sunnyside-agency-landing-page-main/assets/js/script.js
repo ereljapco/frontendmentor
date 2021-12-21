0a9280291a237f59aa7ac6ed7ca61d78fa2740ef
Erel Ropeta <helloerelita@gmail.com>
2021-12-22 06:35:55 +0800

    Added click event on hamburger icon for nav.

---
+++ frontendmentor/junior/sunnyside-agency-landing-page-main/assets/js/script.js
@@ -0,0 +1,19 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const headerNav = document.querySelector('header > nav');
+
+// hamburgerIcon.addEventListener('click', () => {
+//     if (headerNav.style.visibility === 'hidden') {
+//         headerNav.style.visibility = 'visible';
+//     } else {
+//         headerNav.style.visibility = 'hidden';
+//     }
+// });
+
+hamburgerIcon.addEventListener('click', () => {
+    if (headerNav.classList.contains('nav--appear')) {
+        headerNav.classList.remove('nav--appear');
+    } else {
+        headerNav.classList.add('nav--appear');
+    }
+});
+
