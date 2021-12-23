952b7b629a3bc5ce73dce28352b62e901298625b
Erel Ropeta <helloerelita@gmail.com>
2021-12-23 09:23:24 +0800

    Added .header__top in the querySelector for headerNav. and removed unnecessary comments.

--- frontendmentor/junior/sunnyside-agency-landing-page-main/assets/js/script.js
+++ frontendmentor/junior/sunnyside-agency-landing-page-main/assets/js/script.js
@@ -1,14 +1,6 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const headerNav = document.querySelector('header > .header__top > nav');
 
-// hamburgerIcon.addEventListener('click', () => {
-//     if (headerNav.style.visibility === 'hidden') {
-//         headerNav.style.visibility = 'visible';
-//     } else {
-//         headerNav.style.visibility = 'hidden';
-//     }
-// });
-
 hamburgerIcon.addEventListener('click', () => {
     if (headerNav.classList.contains('nav--appear')) {
         headerNav.classList.remove('nav--appear');
