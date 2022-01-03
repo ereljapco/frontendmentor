99b53264432a73164a74f31f129816bbd6c174a2
Erel Ropeta <helloerelita@gmail.com>
2022-01-03 15:44:19 +0800

    Added toggle function for .header__menu's open-close classes.

--- frontendmentor/junior/blogr-landing-page-main/assets/js/script.js
+++ frontendmentor/junior/blogr-landing-page-main/assets/js/script.js
@@ -1,6 +1,9 @@
 const menuIcon = document.querySelector('.header__menu__icon');
+const headerMenu = document.querySelector('.header__menu')
 
 menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('open');
-    menuIcon.classList.toggle('close');
+    menuIcon.classList.toggle('icon--open');
+    menuIcon.classList.toggle('icon--close');
+    headerMenu.classList.toggle('menu--close');
+    headerMenu.classList.toggle('menu--open');
 });
