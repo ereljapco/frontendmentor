fab55977bc871c8ed8ea7dc69aa4c7e7db88ed3a
Erel Ropeta <helloerelita@gmail.com>
2022-01-04 19:43:48 +0800

    Fixed mobile .header__nav event listener for the subnav.

--- frontendmentor/junior/blogr-landing-page-main/assets/js/script.js
+++ frontendmentor/junior/blogr-landing-page-main/assets/js/script.js
@@ -1,9 +1,20 @@
-const menuIcon = document.querySelector('.header__menu__icon');
-const headerMenu = document.querySelector('.header__menu')
+const menuIcon = document.querySelector('.header__nav-icon');
+const headerMenu = document.querySelector('.header__nav')
+let headerNavItem = document.querySelectorAll('.header__nav-item');
 
+// Opens and closes mobile menu
 menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('icon--open');
-    menuIcon.classList.toggle('icon--close');
-    headerMenu.classList.toggle('menu--close');
-    headerMenu.classList.toggle('menu--open');
+    menuIcon.classList.toggle('header__nav-icon--open');
+    menuIcon.classList.toggle('header__nav-icon--close');
+    headerMenu.classList.toggle('header__nav--close');
+    headerMenu.classList.toggle('header__nav--open');
+});
+
+headerNavItem.forEach((e) => {
+    e.querySelector('.header__nav-title').addEventListener('click', () => {
+        e.querySelector('.header__nav-title').classList.toggle('header__nav-title--close');
+        e.querySelector('.header__nav-title').classList.toggle('header__nav-title--open');
+        e.querySelector('.header__subnav-list').classList.toggle('header__subnav-list--close');
+        e.querySelector('.header__subnav-list').classList.toggle('header__subnav-list--open');
+    });
 });
