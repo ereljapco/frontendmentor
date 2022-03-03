fe3e7c8fb89c44aa11aa9d06c34962fb0a8d0812
Erel Ropeta <helloerelita@gmail.com>
2022-03-03 13:52:57 +0800

    Removed duplicated directory.

--- junior/sunnyside-agency-landing-page-main/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-const headerNav = document.querySelector('.nav__mobile');
-
-hamburgerIcon.addEventListener('click', () => {
-    headerNav.classList.toggle('nav--appear');
-});
